5c1adc0929f9a2c926f23bb109ec2258d47953b7
kgtillis <ktillis@hotmail.com>
2021-09-02 00:11:36 -0700

    Hello World Project

---
+++ Hmwk/Hello-World/main.cpp
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Karyssa Tillis
+ *
+ * Created on September 2, 2021, 12:01 AM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World" << endl;
+}
+
